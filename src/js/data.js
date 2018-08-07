aeca592655a1acc852d383f6b0714a6aadf43fc9
zabarca <zareth_abarca@hotmail.com>
2018-08-07 10:04:51 -0500

    Prueba 2 con tarjetas de información

--- src/js/data.js
+++ src/js/data.js
@@ -48,16 +48,42 @@
 //   address:"Venustiano Carranza 49"
 // }
 // };
-const url= "../data/menu.json";
+const url= "https://raw.githubusercontent.com/zabarca/cdmx-2018-06-bc-core-pm-foodmap/master/src/data/menu.json";
+function renderInfo (data) {
+    //let result = "";
+    console.log(Object.values(data)[0]);
+       const info = Object.values(data)[0];
+       let result = "";
+       for(let i=0; i<info.length;i++){
+           console.log(info[i]); //[i] la i da la info de cada nombre
+           result += `<div class="card">
+           <div class="info">
+               <p>Lugar: ${info[i].place}</p>
+               <p>Teléfono: ${info[i].phone}</p>
+               <p>Reseña: ${info[i].review}</p>
+               <p>Dirección: ${info[i].address}</p>
+           </div>
+       </div>`
+}
+        document.getElementById('tarjeta').innerHTML=result;
+        // container.innerHTML=result;
+   //});
+}
+document.getElementById('delete').addEventListener('click', (event) =>{
+  container.innerHTML = '';
+})
 window.onload = () =>{
     fetch(url)
     .then(response => response.json())
     .then(data => {
         console.log(data);
-        renderInfo(data);
-    })
-    .catch(error =>{
-        console.log("error");
-    })
+        let x= document.getElementById("btn");
+        x.addEventListener("click",(event) => {
+      renderInfo(data);
+      });
+    });
+    // .catch(error =>{
+    //      console.log("error");
+    // });
 
-}
+};
